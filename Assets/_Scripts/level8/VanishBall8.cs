7f61cb624f00584ee7bc6502c37d30511fc76eba
alif al amin <alifalamin4@gmail.com>
2017-07-13 16:26:55 +0600

    8 levels

---
+++ Assets/_Scripts/level8/VanishBall8.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanishBall8 : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("BallTag2"))
+        {
+            other.gameObject.SetActive(false);
+
+
+            Application.LoadLevel(17);
+            Debug.Log("Inside method");
+        }
+
+        Debug.Log("outside");
+
+
+    }
+}
+
